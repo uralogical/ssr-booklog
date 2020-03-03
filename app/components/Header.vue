3115f3861fdc29ef9405077dc513d05ba0f2bd6e
M.Atsumi <atsumim@ikyu.com>
2020-03-03 09:33:39 +0900

    :feat: Header のデザイン調整

--- app/components/Header.vue
+++ app/components/Header.vue
@@ -1,9 +1,9 @@
 <template>
-  <el-menu mode='horizontal' :router='true'>
-    <el-menu-item index='1' style='pointer-events:none;'>
-      SSR BookLog
+  <el-menu mode='horizontal' :router='true' class='header'>
+    <el-menu-item class="menuItem" index='1' style='pointer-events:none;'>
+      BookLog
     </el-menu-item>
-    <el-menu-item index='2' :route="{ path: '/posts' }">
+    <el-menu-item class="menuItem" index='2' :route="{ path: '/posts' }">
       本の一覧
     </el-menu-item>
 
@@ -12,19 +12,22 @@
         v-if='user'
         index='4'
         style='float: right;'
-        :route="{ path: `/user/${user.id}` }">
+        class="menuItem"
+        :route="{ path: `/users/${user.id}` }">
         <span>{{ user.id }}</span>
       </el-menu-item>
       <el-menu-item
         v-else
         index='4'
         style='float: right;'
+        class="menuItem"
         :route="{ path: '/' }">
         <span>ログイン</span>
       </el-menu-item>
       <el-menu-item
         index='5'
         style='float: right;'
+        class="menuItem"
         :route="{ path: '/posts/new' }">
         本を登録
       </el-menu-item>
@@ -41,3 +44,12 @@ export default {
   }
 }
 </script>
+<style scoped>
+.header {
+  background-color: #003f5c;
+}
+
+.menuItem {
+  color: #fff;
+}
+</style>
